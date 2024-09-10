fbe01bf1402ef1d765c91cdcc753e82d20e7eead
Tóth Benedek Vitéz <tothvitez@gmail.com>
2024-09-10 19:34:10 +0200

    Added stack template which should be used for all stack implementations.

---
+++ Stack/template/main.c
@@ -0,0 +1,56 @@
+#include "stack.h"
+#include <stdio.h>
+
+static char* strings[] = {
+    "Hello",
+    "World",
+    "This",
+    "Is",
+    "A",
+    "Test",
+    "String",
+    "Array",
+    "For",
+    "Stack",
+    "Implementation",
+    "In",
+    "C"
+};
+
+int main()
+{
+    Stack *stack = initStack(char *);
+
+    if (stack == NULL)
+    {
+        fprintf(stderr, "ERROR: Stack initialization failed!\n");
+        return 1;
+    }
+
+    for (int i = 0; i < sizeof(strings) / sizeof(char *); i++)
+    {
+        push(stack, strings[i]);
+    }
+
+    for (size_t i = 0; i < length(stack); i++)
+    {
+        printf("%s, ", ((char **)stack->data)[i]);
+    }
+    printf("\n");
+    
+    for (size_t i = 0; i < 4; i++)
+    {
+        pop(stack, char*);
+    }
+
+    for (size_t i = 0; i < length(stack); i++)
+    {
+        printf("%s, ", ((char **)stack->data)[i]);
+    }
+    printf("\n");
+
+    printf("Top: %s\n", top(stack, char*));
+
+    deleteStack(stack);
+    return 0;
+}
---
+++ Stack/template/stack.h
@@ -0,0 +1,182 @@
+#ifndef STACK_H
+#define STACK_H
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STACK_INCREMENT_SIZE 10
+
+typedef struct Stack
+{
+    void *data;
+    int currentSize;
+    int maxHeight;
+    size_t elementSize;
+} Stack;
+
+#define initStack(__type) ({ \
+    Stack *__ret; \
+    Stack* __stack = (Stack *)malloc(sizeof(Stack)); \
+    if (__stack == NULL) { \
+        fprintf(stderr, "ERROR: Memory allocation for Stack structure failed!\n"); \
+        __ret = (Stack *)NULL; \
+    } \
+    else { \
+        __stack->data = malloc(STACK_INCREMENT_SIZE * sizeof(__type)); \
+        if (__stack->data == NULL) { \
+            fprintf(stderr, "ERROR: Memory allocation for Stack data failed!\n"); \
+            free(__stack); \
+            __ret = (Stack *)NULL; \
+        } \
+        else { \
+            __stack->currentSize = 0; \
+            __stack->maxHeight = STACK_INCREMENT_SIZE; \
+            __stack->elementSize = sizeof(__type); \
+            __ret = __stack; \
+        } \
+    } \
+    __ret; \
+})
+
+#define deleteStack(__stack) ({ \
+    int __ret = 0; \
+    if (__stack != NULL) { \
+        if (__stack->data != NULL) { \
+            free(__stack->data); \
+        } \
+        free(__stack); \
+        __ret = 1; \
+    } \
+    __ret; \
+})
+
+#define push(__stack, __value) ({ \
+    int __ret; \
+    if (__stack == NULL) { \
+        fprintf(stderr, "ERROR: Stack is not initialized!\n"); \
+        __ret = 0; \
+    } \
+    else if (__stack->elementSize != sizeof(__value)) { \
+        fprintf(stderr, "ERROR: Type mismatch!\n"); \
+        __ret = 0; \
+    } \
+    else { \
+        if (isFull(__stack) > 0) { \
+            int __size = STACK_INCREMENT_SIZE; \
+            void *__temp = realloc(__stack->data, (__stack->maxHeight + __size) * __stack->elementSize); \
+            if (__temp == NULL) { \
+                fprintf(stderr, "ERROR: Memory reallocation failed!\n"); \
+                deleteStack(__stack); \
+                __ret = 0; \
+            } \
+            else { \
+                __stack->data = __temp; \
+                __stack->maxHeight += __size; \
+                ((typeof(__value) *)__stack->data)[__stack->currentSize++] = __value; \
+                __ret = 1; \
+            } \
+        } \
+        else { \
+            ((typeof(__value) *)__stack->data)[__stack->currentSize++] = __value; \
+            __ret = 1; \
+        } \
+    } \
+    __ret; \
+})
+
+#define pop(__stack, __type) ({ \
+    __type __ret; \
+    if (__stack == NULL) { \
+        fprintf(stderr, "ERROR: Stack is not initialized!\n"); \
+        __ret = (__type)NULL; \
+    } \
+    else if (__stack->elementSize != sizeof(__type)) { \
+        fprintf(stderr, "ERROR: Type mismatch!\n"); \
+        __ret = (__type)NULL; \
+    } \
+    else { \
+        if (isEmpty(__stack) > 0) { \
+            __ret = (__type)NULL; \
+        } \
+        else { \
+            __ret = ((__type *)__stack->data)[--__stack->currentSize]; \
+        } \
+    } \
+    __ret; \
+})
+
+#define top(__stack, __type) ({ \
+    __type __ret; \
+    if (__stack == NULL) { \
+        fprintf(stderr, "ERROR: Stack is not initialized!\n"); \
+        __ret = (__type)NULL; \
+    } \
+    else if (__stack->elementSize != sizeof(__type)) { \
+        fprintf(stderr, "ERROR: Type mismatch!\n"); \
+        __ret = (__type)NULL; \
+    } \
+    else { \
+        if (isEmpty(__stack) > 0) \
+        { \
+            __ret = (__type)NULL; \
+        } \
+        else { \
+            __ret = ((__type *)__stack->data)[__stack->currentSize - 1]; \
+        } \
+    } \
+    __ret; \
+})
+
+#define isFull(__stack) ({ \
+    int __ret; \
+    if (__stack == NULL) \
+    { \
+        fprintf(stderr, "ERROR: Stack is not initialized!\n"); \
+        __ret = 0; \
+    } \
+    else { \
+        __ret = __stack->currentSize == __stack->maxHeight; \
+    } \
+    __ret; \
+})
+
+#define isEmpty(__stack) ({ \
+    int __ret; \
+    if (__stack == NULL) \
+    { \
+        fprintf(stderr, "ERROR: Stack is not initialized!\n"); \
+        __ret = 0; \
+    } \
+    else { \
+        __ret = __stack->currentSize == 0; \
+    } \
+    __ret; \
+})
+
+#define setEmpty(__stack) ({ \
+    int __ret; \
+    if (__stack == NULL) \
+    { \
+        fprintf(stderr, "ERROR: Stack is not initialized!\n"); \
+        __ret = 0; \
+    } \
+    else { \
+        __stack->currentSize = 0; \
+        __ret = 1; \
+    } \
+    __ret; \
+})
+
+#define length(__stack) ({ \
+    int __ret; \
+    if (__stack == NULL) \
+    { \
+        fprintf(stderr, "ERROR: Stack is not initialized!\n"); \
+        __ret = 0; \
+    } \
+    else { \
+        __ret = __stack->currentSize; \
+    } \
+    __ret; \
+})
+
+#endif
